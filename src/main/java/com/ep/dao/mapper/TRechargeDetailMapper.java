ab159c054ad3e22ef3ad036e392555b12a99321f
fangchen.chai <1127671368@qq.com>
2017-12-15 12:32:22 +0800

    fix bug

--- src/main/java/com/ep/dao/mapper/TRechargeDetailMapper.java
+++ src/main/java/com/ep/dao/mapper/TRechargeDetailMapper.java
@@ -17,7 +17,7 @@ public interface TRechargeDetailMapper extends BaseMapper<TRechargeDetail, Long,
     @Select("select id from t_recharge_detail where sys_order = #{order}")
     Long selectOrderIdByOrder(@Param("order") String order);
 
-    @Update("update t_recharge_detail set pay_status = 2 and date = now()  where id = #{orderId}")
+    @Update("update t_recharge_detail set pay_status = 2 ,date = now()  where id = #{orderId}")
     Integer updatePayedById(@Param("orderId") Long orderId);
     @Update("update t_recharge_detail set out_pay_code = #{outOrderCode} where id = #{orderId}")
     Integer updateOutPayCodeById(@Param("outOrderCode") String outOrderCode, @Param("orderId") Long orderId);
